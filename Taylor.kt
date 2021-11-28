fecd5f65540fada1526f46feb9be736e2ecf0718
kuba6671 <33839399+kuba6671@users.noreply.github.com>
2021-11-28 13:26:36 +0100

    Update Taylor.kt

--- Taylor.kt
+++ Taylor.kt
@@ -1,18 +1,14 @@
-import java.util.stream.DoubleStream
-
-class Taylor internal constructor(var x: DoubleArray, var y: DoubleArray) {
-    fun TaylorDerivative() {
+class Taylor internal constructor(private var x: DoubleArray,private var y: DoubleArray) {
+    fun taylorDerivative() {
         val h = x[1] - x[0]
         val x0 = x[x.size - 1]
-        val dfx = Array(5) {
-            arrayOfNulls<Double>(6) }
+        val dfx = Array(x.size) {
+            arrayOfNulls<Double>(x.size+1) }
         for (i in x.indices) {
             for (j in 0 until x.size + 1) dfx[i][j] = 0.0
         }
         for (i in x.indices) {
             dfx[i][0] = x[i]
-        }
-        for (i in x.indices) {
             dfx[i][1] = y[i]
         }
         for (i in 1 until x.size) {
@@ -20,11 +16,6 @@ class Taylor internal constructor(var x: DoubleArray, var y: DoubleArray) {
                 dfx[i][j] = dfx[i][j - 1]?.minus(dfx[i - 1][j - 1]!!)
             }
         }
-         /*for(i in 1 until x.size) {
-            for (j in 1 until x.size+1 )
-                System.out.printf("%.5f ", dfx[i][j]);
-            System.out.println();
-        }*/
         var result = 0.0
         var denominator = 1.0
         for (j in 2 until x.size + 1) {
@@ -32,13 +23,23 @@ class Taylor internal constructor(var x: DoubleArray, var y: DoubleArray) {
             denominator += 1.0
         }
         result *= (x0 / h)
-        System.out.printf("Result: %.4f ", result)
+        System.out.printf("Result: %.4f\n", result)
     }
 }
-        fun main(args: Array<String>) {
-            val x = doubleArrayOf(0.6, 0.7, 0.8, 0.9, 1.0)
-            val y = doubleArrayOf(-0.51083, -0.35667, -0.22314, -0.10536, 0.0)
+        fun main() {
+            val x = doubleArrayOf(1.3,1.6,1.9,2.2,2.5,2.8,3.1)
+            val y = doubleArrayOf(0.970547,1.01197,1.063698,1.107149,1.144169,1.176005,1.203622)
             val t1 = Taylor(x, y)
-            t1.TaylorDerivative()
+            println("x="+x.contentToString())
+            println("y="+y.contentToString())
+            t1.taylorDerivative()
+
+            println()
+            val x2 = doubleArrayOf(1.3,1.6,1.9,2.2,2.5,2.8,3.1,3.4,3.7)
+            val y2 = doubleArrayOf(0.970547,1.01197,1.063698,1.107149,1.144169,1.176005,1.203622, 1.233143, 1.270312)
+            val t2 = Taylor(x2, y2)
+            println("x2="+x2.contentToString())
+            println("y2="+y2.contentToString())
+            t2.taylorDerivative()
         }
 
